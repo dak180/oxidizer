f637d5dbd8415992ef00a6e9693373df40bf7109
vargol <vargol>
2006-06-22 18:03:10 +0000

    GPL file headers

--- GenomeManagedObject.h
+++ GenomeManagedObject.h
@@ -1,10 +1,7 @@
-//
-//  GenomeManagedObject.h
-//  oxidizer
-//
-//  Created by David Burnett on 26/02/2006.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
 /*
+    oxidizer - cosmic recursive fractal flames
+    Copyright (C) 2006  David Burnett <vargol@ntlworld.com>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -19,6 +16,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
+
 #import <Cocoa/Cocoa.h>
 
 
