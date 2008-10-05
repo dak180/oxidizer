731ccf31eb305f4dba4e0aa951fb0c0d747186d2
vargol <vargol>
2008-10-05 22:00:59 +0000

    Value transformers for 'Movie as Stills' dialogue.

---
+++ ImageFormatIsPNG.h
@@ -0,0 +1,16 @@
+//
+//  ImageFormatToHideTransform.h
+//  oxidizer
+//
+//  Created by David Burnett on 05/10/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ImageFormatIsPNG : NSValueTransformer {
+
+}
+
+@end
---
+++ ImageFormatIsPNG.m
@@ -0,0 +1,33 @@
+//
+//  ImageFormatToHideTransform.m
+//  oxidizer
+//
+//  Created by David Burnett on 05/10/2008.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageFormatIsPNG.h"
+
+
+@implementation ImageFormatIsPNG
+
++ (Class)transformedValueClass { return [NSNumber class]; }
+
++ (BOOL)allowsReverseTransformation { return NO; }
+
+- (id)transformedValue:(id)value {
+	
+	NSString *format = (NSString *)value;
+	if ([format compare:@"PNG"] == 0 ) {
+				
+		return [NSNumber numberWithBool:NO];
+	} 
+	
+	return[NSNumber numberWithBool:YES];
+	
+	
+}
+
+
+
+@end
