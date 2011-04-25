f77912f11cc68e4db981db19d8436f9d5fdad890
dak180 <dak180@users.sourceforge.net>
2011-04-25 13:25:10 -0400

    New and improved autorevision.sh.

--- Configs/autorevision.sh
+++ Configs/autorevision.sh
@@ -1,52 +1,106 @@
 #!/bin/bash
 
-# autorevision.sh - a hacked up shellscript to get git revisions etc. into binary builds.
-# Defines are named as they are for historical reasons.
+# autorevision.sh - a shell script to get git / hg revisions etc. into binary builds.
 # To use pass a path to the desired output file: some/path/to/autorevision.h.
+# Note: the script will run at the root level of the repository that it is in.
 
+# Config
 TARGETFILE="${1}"
 
-cd "${SRCROOT}"
+# For git repos
+function gitRepo {
+    cd "$(git rev-parse --show-toplevel)"
 
-# Is the working copy clean?
-git diff --quiet HEAD &> /dev/null
-WC_MODIFIED="${?}"
+    # Is the working copy clean?
+    git diff --quiet HEAD &> /dev/null
+    WC_MODIFIED="${?}"
 
-# Enumeration of changesets
-GIT_NUM="$(git rev-list --count HEAD)"
+    # Enumeration of changesets
+    VCS_NUM="$(git rev-list --count HEAD)"
+    if [ -z "${VCS_NUM}" ]; then
+        echo "warning: Counting the number of revisions may be slower due to an outdated git version less than 1.7.2.3. If something breaks, please update it."
+        VCS_NUM="$(git rev-list HEAD | wc -l)"
+    fi
 
-# The full revision hash
-GIT_FULL_HASH="$(git rev-parse HEAD)"
+    # The full revision hash
+    VCS_FULL_HASH="$(git rev-parse HEAD)"
 
-# The short hash
-GIT_SHORT_HASH="$(echo ${GIT_FULL_HASH} | cut -b 1-7)"
+    # The short hash
+    VCS_SHORT_HASH="$(echo ${VCS_FULL_HASH} | cut -b 1-7)"
 
-# Current branch
-GIT_URI="$(git symbolic-ref HEAD)"
+    # Current branch
+    VCS_URI="$(git symbolic-ref HEAD)"
 
-# Current tag (or branch if there is no tag)
-GIT_TAG="$(git describe --exact-match --tags 2>/dev/null)"
-if [ -z "${GIT_TAG}" ]; then
-	GIT_TAG="${GIT_URI}"
+    # Current tag (or uri if there is no tag)
+    VCS_TAG="$(git describe --exact-match --tags 2>/dev/null)"
+    if [ -z "${VCS_TAG}" ]; then
+        VCS_TAG="${VCS_URI}"
+    fi
+
+    # Date of the curent commit
+    VCS_DATE="$(git log -1 --pretty=format:%ci)"
+}
+
+# For hg repos
+function hgRepo {
+    cd "$(hg root)"
+
+    # Is the working copy clean?
+    hg sum | grep -q 'commit: (clean)'
+    WC_MODIFIED="${?}"
+
+    # Enumeration of changesets
+    VCS_NUM="$(hg id -n)"
+
+    # The full revision hash
+    VCS_FULL_HASH="$(hg log -r ${VCS_NUM} -l 1 --template '{node}\n')"
+
+    # The short hash
+    VCS_SHORT_HASH="$(hg id -i)"
+
+    # Current bookmark (bookmarks are roughly equivalent to git's branches) or branch if no bookmark
+    VCS_URI="$(hg id -B | cut -d ' ' -f 1)"
+    # Fall back to the branch if there are no bookmarks
+    if [ -z "${VCS_URI}" ]; then
+        VCS_URI="$(hg id -b)"
+    fi
+
+    # Current tag (or uri if there is no tag)
+    if [ "$(hg log -r ${VCS_NUM} -l 1 --template '{latesttagdistance}\n')" = "0" ]; then
+        VCS_TAG="`hg id -t | sed -e 's:qtip::' -e 's:tip::' -e 's:qbase::' -e 's:qparent::' -e "s:$(hg --color never qtop 2>/dev/null)::" | cut -d ' ' -f 1`"
+    else
+        VCS_TAG="${VCS_URI}"
+    fi
+
+    # Date of the curent commit
+    VCS_DATE="$(hg log -r ${VCS_NUM} -l 1 --template '{date|isodatesec}\n')"
+}
+
+
+if [[ ! -z "$(hg id 2>/dev/null)" ]]; then
+    hgRepo
+elif [[ ! -z "$(git rev-parse HEAD 2>/dev/null)" ]]; then
+    gitRepo
+else
+    echo "error: No repo detected."
+    exit 1
 fi
 
-# Date of the last commit
-GIT_DATE="$(git log -1 --pretty=format:%ci)"
 
 cat > "${TARGETFILE}" << EOF
-/* ${GIT_FULL_HASH} */
+/* ${VCS_FULL_HASH} */
 #ifndef AUTOREVISION_H
 #define AUTOREVISION_H
 
-#define GIT_NUM			${GIT_NUM}
-#define GIT_DATE		${GIT_DATE}
-#define GIT_URI			${GIT_URI}
-#define GIT_TAG			${GIT_TAG}
+#define VCS_NUM         ${VCS_NUM}
+#define VCS_DATE        ${VCS_DATE}
+#define VCS_URI         ${VCS_URI}
+#define VCS_TAG         ${VCS_TAG}
 
-#define GIT_FULL_HASH					${GIT_FULL_HASH}
-#define GIT_SHORT_HASH					${GIT_SHORT_HASH}
+#define VCS_FULL_HASH                   ${VCS_FULL_HASH}
+#define VCS_SHORT_HASH                  ${VCS_SHORT_HASH}
 
-#define GIT_WC_MODIFIED					${WC_MODIFIED}
+#define VCS_WC_MODIFIED                 ${WC_MODIFIED}
 
 #endif
 
